f36300c86566517d5bb1a77c859dd8c7fe5b0a87
José Medeiros <joseprsm@gmail.com>
2025-06-30 16:20:15 +0100

    chore: rename _model to _models

--- src/homematch/__init__.py
+++ src/homematch/__init__.py
@@ -1,5 +1,5 @@
-from ._model import LLM as LLM
-from ._model import Embeddings as Embeddings
+from ._models import LLM as LLM
+from ._models import Embeddings as Embeddings
 
 TEXT_FIELDS = ["description", "neighborhood_description"]
 METADATA_FIELDS = ["neighborhood", "price", "bedrooms", "bathrooms", "size"]
