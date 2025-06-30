895d8a9f91f76324b69148986f353165ae210ff8
José Medeiros <joseprsm@gmail.com>
2025-06-30 20:23:23 +0100

    fix: set OPENAI_API_KEY environment variable if not set

--- src/homematch/_models.py
+++ src/homematch/_models.py
@@ -6,7 +6,10 @@ from openai import OpenAI
 from homematch.utils import generate_random_string
 
 OPENAI_API_BASE = os.getenv("OPENAI_API_BASE", "http://localhost:11434/v1")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", generate_random_string())
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+if OPENAI_API_KEY is None:
+    OPENAI_API_KEY = generate_random_string()
+    os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY  # yes. this is required.
 
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "granite-embedding")
 CHAT_MODEL = os.getenv("CHAT_MODEL", "qwen3:4b")
@@ -14,6 +17,7 @@ CHAT_MODEL = os.getenv("CHAT_MODEL", "qwen3:4b")
 _client = OpenAI(api_key=OPENAI_API_KEY, base_url=OPENAI_API_BASE)
 
 model = ChatOpenAI(model=CHAT_MODEL, client=_client.chat.completions)
+
 embeddings = OpenAIEmbeddings(
     model=EMBEDDING_MODEL,
     client=_client.embeddings,
