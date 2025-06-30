1384c62685536a66d92f087128c4c1d0c5b7b6fa
José Medeiros <joseprsm@gmail.com>
2025-06-30 14:32:47 +0100

    chore: add TEXT_FIELDS and METADATA_FIELDS constants to __init__.py

--- src/homematch/__init__.py
+++ src/homematch/__init__.py
@@ -1,2 +1,5 @@
 from ._model import LLM as LLM
 from ._model import Embeddings as Embeddings
+
+TEXT_FIELDS = ["description", "neighborhood_description"]
+METADATA_FIELDS = ["neighborhood", "price", "bedrooms", "bathrooms", "size"]
