e7385e28546646d1f4df1347716aa54f5b9a7f83
José Medeiros <joseprsm@gmail.com>
2025-06-30 12:41:51 +0100

    feat: add schemas for Neighborhood and Listing models

---
+++ src/homematch/schemas.py
@@ -0,0 +1,35 @@
+import os
+
+from pydantic import BaseModel, Field
+
+
+class Neighborhood(BaseModel):
+    name: str = Field(description="The name of the neighborhood")
+    description: str = Field(description="A brief description of the neighborhood")
+    quirks: list[str] = Field(
+        description="Unique characteristics or quirks of the neighborhood"
+    )
+
+
+class Neighborhoods(BaseModel):
+    root: list[Neighborhood] = Field(
+        description="A list of neighborhoods, each with a name, description, and unique quirks"
+    )
+
+
+class Listing(BaseModel):
+    description: str = Field(description="A description of the real estate listing")
+    price: int = Field(
+        description=f"The price of the listing in {os.environ.get('CURRENCY', 'EUR')}"
+    )
+    bedrooms: int = Field(description="The number of bedrooms in the listing")
+    bathrooms: int = Field(description="The number of bathrooms in the listing")
+    size: int = Field(
+        description=f"The size of the listing in squared {os.environ.get('SIZE_UNIT', 'meters')}"
+    )
+
+
+class Listings(BaseModel):
+    root: list[Listing] = Field(
+        description="A list of real estate listings, each with a description, price, number of bedrooms, bathrooms, and size"
+    )
