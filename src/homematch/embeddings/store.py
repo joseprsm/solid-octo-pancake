5186034fa7d7ae0455a0486ef38c39b8372680c7
José Medeiros <joseprsm@gmail.com>
2025-06-30 18:26:03 +0100

    feat: create VectorStore class with add_listing method

--- src/homematch/embeddings/store.py
+++ src/homematch/embeddings/store.py
@@ -4,23 +4,32 @@ from langchain_chroma import Chroma
 from langchain_core.documents import Document
 
 from homematch import METADATA_FIELDS, TEXT_FIELDS, Embeddings
-from homematch.schemas import Listing
+from homematch.schemas import Listing, Listings
 
 
-def convert_to_document(listing: Listing) -> Document:
-    def get_data(obj, fields):
-        return {field: obj[field] for field in fields if field in obj}
+class VectorStore(Chroma):
+    def __init__(
+        self,
+        *,
+        collection_name: str,
+        embedding_fields: list[str] = None,
+        metadata_fields: list[str] = None,
+        **kwargs,
+    ):
+        self.embedding_fields = embedding_fields or TEXT_FIELDS
+        self.metadata_fields = metadata_fields or METADATA_FIELDS
+        super().__init__(
+            embedding_function=Embeddings(), collection_name=collection_name**kwargs
+        )
 
-    page_content = get_data(listing, TEXT_FIELDS)
-    metadata = get_data(listing, METADATA_FIELDS)
-    return Document(page_content=json.dumps(page_content), metadata=metadata)
+    def add_listing(self, listings: Listings):
+        documents = list(map(self._convert_to_document, listings))
+        return self.add_documents(documents)
 
+    def _convert_to_document(self, listing: Listing):
+        def get_data(obj, fields):
+            return {field: obj[field] for field in fields if field in obj}
 
-def construct_vector_store(
-    collection_name: str = "listings", persist_directory: str = "./chroma"
-) -> None:
-    return Chroma(
-        embedding_function=Embeddings(),
-        persist_directory=persist_directory,
-        collection_name=collection_name,
-    )
+        page_content = get_data(listing, self.embedding_fields)
+        metadata = get_data(listing, self.metadata_fields)
+        return Document(page_content=json.dumps(page_content), metadata=metadata)
